09cf10c8b8af8fe198719a616f027c175083eda5
Kristian Ivanov <kriskopk@abv.bg>
2022-03-01 20:20:43 +0200

    Created Actor model with the needed properties.

---
+++ MovieTickets/MovieTickets.Data/Models/Actor.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MovieTickets.Data.Models
+{
+    public class Actor
+    {
+        [Key]
+        public int ActorId { get; set; }
+
+        [Required]
+        public string ProfilePicture { get; set; }
+
+        [Required]
+        public string FullName { get; set; }
+
+        [Required]
+        public string Bio { get; set; }
+    }
+}

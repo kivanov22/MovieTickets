88bf514960331a1cf08a6633eb9da3a58b3f8777
Kristian Ivanov <kriskopk@abv.bg>
2022-03-01 20:21:56 +0200

    Created db context in the data class library.

---
+++ MovieTickets/MovieTickets.Data/MovieTicketsDbContext.cs
@@ -0,0 +1,13 @@
+﻿namespace MovieTickets.Data
+{
+    using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore;
+
+    public class MovieTicketsDbContext: IdentityDbContext
+    {
+        public MovieTicketsDbContext(DbContextOptions<MovieTicketsDbContext> options)
+           : base(options)
+        {
+        }
+    }
+}

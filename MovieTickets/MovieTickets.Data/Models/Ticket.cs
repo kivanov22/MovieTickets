06b6f4872740c4f27d793bff2547265c97e591aa
Kristian Ivanov <kriskopk@abv.bg>
2022-03-02 19:19:35 +0200

    Created a Ticket model.

---
+++ MovieTickets/MovieTickets.Data/Models/Ticket.cs
@@ -0,0 +1,29 @@
+﻿using MovieTickets.Data.Models.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace MovieTickets.Data.Models
+{
+    public class Ticket
+    {
+        [Key]
+        public int TicketId { get; set; }
+
+        [Required]
+        public string MovieName { get; set; }
+
+        public Resolution Resolution { get; set; }
+
+        public decimal Price { get; set; }
+
+        public TicketType TicketType { get; set; }
+
+        public int Quantity { get; set; }
+
+        public int Row { get; set; }
+
+        [Required]
+        public int Seat { get; set; }
+
+        public int Hall { get; set; }
+    }
+}

36215169641c585cd65c32bc0ec98ac6287457cd
Kristian Ivanov <kriskopk@abv.bg>
2022-03-02 19:19:05 +0200

    Reworked some properties of the model.

--- MovieTickets/MovieTickets.Data/Models/Movie.cs
+++ MovieTickets/MovieTickets.Data/Models/Movie.cs
@@ -14,14 +14,17 @@ namespace MovieTickets.Data.Models
         [Required]
         public string ImageUrl { get; set; }
 
-        public string TrailerPath { get; set; }
+        public DateTime StartDate { get; set; }
 
-        public string ImgdbLink { get; set; }
+        public DateTime EndDate { get; set; }
+
+        //public string TrailerPath { get; set; }
+
+        //public string ImgdbLink { get; set; }
 
         [Required]
         public string Description { get; set; }
 
-        public decimal Price { get; set; }
 
         [Required]
         public string Language { get; set; }
@@ -32,9 +35,12 @@ namespace MovieTickets.Data.Models
 
         public Resolution Resolution { get; set; }
 
-        public Actor Actor { get; set; }
+        public decimal Price { get; set; }
 
-        public Producer Producer { get; set; }
+        public int ProducerId { get; set; }
+        public virtual Producer Producer { get; set; }
+
+        public virtual ICollection<Actor> Actors { get; set; } = new List<Actor>();
 
     }
 }

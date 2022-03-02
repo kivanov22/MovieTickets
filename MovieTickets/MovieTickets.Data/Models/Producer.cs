8a52e23a5a08cba8982dfc7b76cff05c7b12c2b9
Kristian Ivanov <kriskopk@abv.bg>
2022-03-02 14:58:29 +0200

    Created a producer entity

---
+++ MovieTickets/MovieTickets.Data/Models/Producer.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MovieTickets.Data.Models
+{
+    public class Producer
+    {
+        [Key]
+
+        public int ProducerId { get; set; }
+
+        [Required]
+        public string ProfilePicture { get; set; }
+
+        [Required]
+        public string FullName { get; set; }
+
+        [Required]
+        public string Biography { get; set; }
+
+
+    }
+}

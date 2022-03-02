68923453c9dc1dc7d8a8a838ff8e3f4489298c11
Kristian Ivanov <kriskopk@abv.bg>
2022-03-02 17:24:06 +0200

    Created a enumeration with different genres.

---
+++ MovieTickets/MovieTickets.Data/Models/Enums/Genre.cs
@@ -0,0 +1,11 @@
+﻿namespace MovieTickets.Data.Models.Enums
+{
+    public enum Genre
+    {
+        Action = 1,
+        Comedy = 2,
+        Horror = 3,
+        Adventures=4,
+        Fantasy =5,
+    }
+}

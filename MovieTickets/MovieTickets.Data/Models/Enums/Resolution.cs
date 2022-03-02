9f432321ac960d5658df777d76493b650650f957
Kristian Ivanov <kriskopk@abv.bg>
2022-03-02 17:24:28 +0200

    Created a enumeration with resolution types.

---
+++ MovieTickets/MovieTickets.Data/Models/Enums/Resolution.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MovieTickets.Data.Models.Enums
+{
+    public enum Resolution
+    {
+
+        [Display(Name = "2D")]
+        TwoD = 1,
+        [Display(Name = "3D")]
+        ThreeD = 2,
+        [Display(Name = "4KX")]
+        FourKX = 3,
+    }
+}

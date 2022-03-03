3bc5cb57d5a09bb9e82c6c4bee203b71514c606e
Kristian Ivanov <kriskopk@abv.bg>
2022-03-03 20:17:38 +0200

    Added default identity ApplicationUser which i created in models.And added roles to the identity

--- MovieTickets/MovieTickets/Program.cs
+++ MovieTickets/MovieTickets/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using MovieTickets.Data;
+using MovieTickets.Data.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,7 +11,8 @@ builder.Services.AddDbContext<MovieTicketsDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<MovieTicketsDbContext>();
 builder.Services.AddControllersWithViews();
 

2d1767e130099c48f8d3a90d1c676995f92d281d
Kristian Ivanov <kriskopk@abv.bg>
2022-03-03 21:33:42 +0200

    Added db sets for new models and created a mapping table connection.

--- MovieTickets/MovieTickets.Data/MovieTicketsDbContext.cs
+++ MovieTickets/MovieTickets.Data/MovieTicketsDbContext.cs
@@ -4,17 +4,36 @@
     using Microsoft.EntityFrameworkCore;
     using MovieTickets.Data.Models;
 
-    public class MovieTicketsDbContext: IdentityDbContext
+    public class MovieTicketsDbContext : IdentityDbContext<ApplicationUser>
     {
         public MovieTicketsDbContext(DbContextOptions<MovieTicketsDbContext> options)
            : base(options)
         {
         }
 
-        public DbSet<Actor> Actors { get; set; }
-        public DbSet<Cinema> Cinemas { get; set; }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<MovieActor>()
+                 .HasKey(ma => new { ma.ActorId, ma.MovieId });
+
+            builder.Entity<MovieActor>().HasOne(m=>m.Movie).WithMany(ma=>ma.MovieActors).HasForeignKey(ma=>ma.MovieId);
+            builder.Entity<MovieActor>().HasOne(m=>m.Actor).WithMany(ma=>ma.MovieActors).HasForeignKey(ma=>ma.ActorId);
+
+            base.OnModelCreating(builder);
+        }
         public DbSet<Producer> Producers { get; set; }
-        public DbSet<Ticket> Tickets { get; set; }
+        public DbSet<Actor> Actors { get; set; }
+
+        public DbSet<MovieActor> MovieActors { get; set; }
         public DbSet<Movie> Movies { get; set; }
+        public DbSet<Cinema> Cinemas { get; set; }
+        public DbSet<Ticket> Tickets { get; set; }
+
+        //orders
+        public DbSet<Order> Orders { get; set; }
+
+        public DbSet<OrderItem> OrderItems { get; set; }
+
+        public DbSet<ShoppingCartItem> ShoppingCartItems { get; set; }
     }
 }

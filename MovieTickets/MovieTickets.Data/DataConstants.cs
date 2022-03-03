ef6412a3021e6fec2984d6251cc27e1bf8bc388d
Kristian Ivanov <kriskopk@abv.bg>
2022-03-03 21:31:47 +0200

    Changed some values of constants and added new ones.

--- MovieTickets/MovieTickets.Data/DataConstants.cs
+++ MovieTickets/MovieTickets.Data/DataConstants.cs
@@ -4,18 +4,22 @@
     {
         //Actors and Producers
 
-        public const int FullNameMaxLength = 30;
-        public const int FullNameMinLength = 10;
+        public const int FullNameMinLength = 3;
+        public const int FullNameMaxLength = 50;
         public const int AgeMaxLength = 80;
         public const int AgeMinLength = 10;
         public const int BiographyMinLength = 10;
         public const int BiographyMaxLength = 255;
 
+        public const string ActorPictureError = "Profile picture is required";
+        public const string ActorFullNameError = "Full Name is required";
+        public const string ActorBiographyError = "Biography is required";
+
         //Movie
         public const int MovieNameMinLength = 10;
         public const int MovieNameMaxLength = 40;
-        public const int DescriptionMaxLength = 255;
         public const int DescriptionMinLength = 10;
+        public const int DescriptionMaxLength = 255;
         //price validation
         //date validation
 
@@ -25,14 +29,18 @@
         public const int CinemaNameMaxLenght = 20;
         public const int CityNameMinLenght = 10;
         public const int CityNameMaxLenght = 20;
-        public const int CinemaAddressMaxLenght = 40;
         public const int CinemaAddressMinLenght = 20;
+        public const int CinemaAddressMaxLenght = 40;
         public const int CinemaHallMinLength = 1;
         public const int CinemaHallMaxLength = 10;
-        public const int CinemaRowMaxLength = 20;
         public const int CinemaRowMinLength = 1;
+        public const int CinemaRowMaxLength = 20;
         public const int CinemaSeatMinLength = 1;
         public const int CinemaSeatMaxLength = 50;
+
+        public const string NameError = "Name is required";
+        public const string DescriptionError = "Description is required";
+        
 
 
         //Tickets

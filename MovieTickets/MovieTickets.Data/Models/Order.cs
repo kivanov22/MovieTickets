a5d39216a959507a668df20e8e1593c26f193c55
Kristian Ivanov <kriskopk@abv.bg>
2022-03-03 21:27:12 +0200

    Created a order model.

---
+++ MovieTickets/MovieTickets.Data/Models/Order.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MovieTickets.Data.Models
+{
+    public class Order
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Email { get; set; }
+
+        public string UserId { get; set; }
+
+        public ApplicationUser User { get; set; }
+
+        public virtual List<OrderItem> OrderItems { get; set; } = new List<OrderItem>();
+    }
+}

f4ad0ddc10d23457efa2c3a189cc60e03d5caba8
Kristian Ivanov <kriskopk@abv.bg>
2022-03-03 21:27:35 +0200

    Created a order item model.

---
+++ MovieTickets/MovieTickets.Data/Models/OrderItem.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MovieTickets.Data.Models
+{
+    public class OrderItem
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int Quantity { get; set; }
+
+        public int MovieId { get; set; }
+        [ForeignKey("MovieId")]
+        public Movie Movie { get; set; }
+
+        public int OrderId { get; set; }
+        [ForeignKey("OrderId")]
+
+        public Order Order { get; set; }
+    }
+}

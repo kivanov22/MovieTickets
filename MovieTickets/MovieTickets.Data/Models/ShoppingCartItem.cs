8a320e2557ff01f7e1fbcf718499cd2dd2dee85f
Kristian Ivanov <kriskopk@abv.bg>
2022-03-03 21:31:19 +0200

    Created a ShoppingcartItem model.

---
+++ MovieTickets/MovieTickets.Data/Models/ShoppingCartItem.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MovieTickets.Data.Models
+{
+    public class ShoppingCartItem
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public Movie Movie { get; set; }
+
+        public int Quantity { get; set; }
+
+        public string ShoppingCartId { get; set; }
+    }
+}

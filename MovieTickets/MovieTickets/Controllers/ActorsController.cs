8d7dc60174cf16286592351323b6f03e5b99cf20
Kristian Ivanov <kriskopk@abv.bg>
2022-03-04 21:26:50 +0200

    Created a actor controller to test view which returns.

--- MovieTickets/MovieTickets/Controllers/ActorsController.cs
+++ MovieTickets/MovieTickets/Controllers/ActorsController.cs
@@ -1,6 +1,23 @@
-﻿namespace MovieTickets.Web.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieTickets.Data;
+
+namespace MovieTickets.Web.Controllers
 {
-    public class ActorsController
+    public class ActorsController:Controller
     {
+        private readonly MovieTicketsDbContext data;
+
+        public ActorsController(MovieTicketsDbContext data)
+        {
+            this.data = data;
+        }
+
+        public IActionResult Index()
+        {
+            var actors = data.Actors.ToList();
+
+            return View(actors);
+        }
+
     }
 }

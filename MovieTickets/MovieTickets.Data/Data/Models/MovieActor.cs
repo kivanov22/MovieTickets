4a21c69adec20a27b0ac0b6700ad0fed534c7bb1
Kristian Ivanov <kriskopk@abv.bg>
2022-03-05 21:37:50 +0200

    Added virtual to the objects movie and actor

--- MovieTickets/MovieTickets.Data/Data/Models/MovieActor.cs
+++ MovieTickets/MovieTickets.Data/Data/Models/MovieActor.cs
@@ -4,11 +4,11 @@
     {
         public int MovieId { get; set; }
 
-        public Movie Movie { get; set; }
+        public virtual Movie Movie { get; set; }
 
         public int ActorId { get; set; }
 
-        public Actor Actor { get; set; }
+        public virtual Actor Actor { get; set; }
 
     }
 }

726df3653627921439e64ddcf5491680356bbc87
Kristian Ivanov <kriskopk@abv.bg>
2022-03-08 15:52:28 +0200

    Created a login view model with email and password.

---
+++ MovieTickets/MovieTickets/ViewModels/Account/LoginViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MovieTickets.Web.ViewModels.Account
+{
+    using static Data.DataConstants;
+    public class LoginViewModel
+    {
+        [Display(Name =DisplayEmail)]
+        [Required(ErrorMessage =EmailError)]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}

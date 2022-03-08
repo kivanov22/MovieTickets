ffbce87ea4f2c687ea6d4060fa65491ab1032be4
Kristian Ivanov <kriskopk@abv.bg>
2022-03-08 15:53:19 +0200

    Created a register view model with email,fullname,password,confirm password.

---
+++ MovieTickets/MovieTickets/ViewModels/Account/RegisterViewModel.cs
@@ -0,0 +1,25 @@
+﻿namespace MovieTickets.Web.ViewModels.Account
+{
+    using System.ComponentModel.DataAnnotations;
+    using static Data.DataConstants;
+    public class RegisterViewModel
+    {
+        [Display(Name = DisplayFullName )]
+        [Required(ErrorMessage = FullNameError)]
+        public string FullName { get; set; }
+
+        [Display(Name = DisplayEmail)]
+        [Required(ErrorMessage = EmailError)]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name = DisplayConfirmPassword)]
+        [Required(ErrorMessage = ConfirmPasswordError)]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Passwords do not match")]
+        public string ConfirmPassword { get; set; }
+    }
+}

d2ab5fecad2e9130a6396eee8f597fe384311414
Kristian Ivanov <kriskopk@abv.bg>
2022-03-08 16:02:50 +0200

    Created a order view model.

---
+++ MovieTickets/MovieTickets/ViewModels/Orders/OrderViewModel.cs
@@ -0,0 +1,20 @@
+﻿using MovieTickets.Data.Models;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MovieTickets.Web.ViewModels.Orders
+{
+    public class OrderViewModel
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Email { get; set; }
+
+        public string UserId { get; set; }
+        [ForeignKey(nameof(UserId))]
+        public ApplicationUser User { get; set; }
+
+        public List<OrderItem> OrderItems { get; set; }
+    }
+}

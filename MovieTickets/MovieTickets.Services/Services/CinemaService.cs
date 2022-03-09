fc5391a06823ad3c814f51f06f4db37eeaade375
Kristian Ivanov <kriskopk@abv.bg>
2022-03-09 21:54:45 +0200

    Created Cinema service which inherits the EntityBaseRepository with Cinema model , and ICinemaService interface. Repository provides the default logic for create,delete,edit,get all , get by id.

--- MovieTickets/MovieTickets.Services/Services/CinemaService.cs
+++ MovieTickets/MovieTickets.Services/Services/CinemaService.cs
@@ -1,6 +1,15 @@
-﻿namespace MovieTickets.Services.Services
+﻿using MovieTickets.Data;
+using MovieTickets.Data.Data.Common;
+using MovieTickets.Data.Models;
+using MovieTickets.Services.Contracts;
+
+namespace MovieTickets.Services.Services
 {
-    public class CinemaService
+    public class CinemaService : EntityBaseRepository<Cinema>,ICinemaService
     {
+        public CinemaService(MovieTicketsDbContext context) :base(context)
+        {
+
+        }
     }
 }

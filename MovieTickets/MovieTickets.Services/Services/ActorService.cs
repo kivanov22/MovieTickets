5b8d5754e3acfab2574217ce4ddac635d6549f94
Kristian Ivanov <kriskopk@abv.bg>
2022-03-09 21:50:23 +0200

    Created Actor service which inherits EntityBaseRepository from Actor and IActorService. So it inherits from the base repository the logic for create,get all , get by id,delete actor.

--- MovieTickets/MovieTickets.Services/Services/ActorService.cs
+++ MovieTickets/MovieTickets.Services/Services/ActorService.cs
@@ -1,6 +1,15 @@
-﻿namespace MovieTickets.Services.Services
+﻿using MovieTickets.Data;
+using MovieTickets.Data.Data.Common;
+using MovieTickets.Data.Models;
+using MovieTickets.Services.Contracts;
+
+namespace MovieTickets.Services.Services
 {
-    public class ActorService
+    public class ActorService :EntityBaseRepository<Actor>, IActorService
     {
+
+        public ActorService(MovieTicketsDbContext context):base(context){ }
+
+       
     }
 }

1a6b55800561078bc90afb76dcbc8d3a63927d65
Kristian Ivanov <kriskopk@abv.bg>
2022-03-10 14:02:45 +0200

    Created the producer service which inherits the EntityBaseRepository with producer, and IProducerService interface. By inherting the repository we get the create,update,delete ,get all producers logic.

--- MovieTickets/MovieTickets.Services/Services/ProducerService.cs
+++ MovieTickets/MovieTickets.Services/Services/ProducerService.cs
@@ -1,6 +1,13 @@
-﻿namespace MovieTickets.Services.Services
+﻿using MovieTickets.Data;
+using MovieTickets.Data.Data.Common;
+using MovieTickets.Data.Models;
+using MovieTickets.Services.Contracts;
+
+namespace MovieTickets.Services.Services
 {
-    public class ProducerService
+    public class ProducerService: EntityBaseRepository<Producer>, IProducerService
     {
+        public ProducerService(MovieTicketsDbContext context): base(context) { }
+        
     }
 }

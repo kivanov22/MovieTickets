c7f752938196ddfb577d56d29b5fe5bb7d44e523
Kristian Ivanov <kriskopk@abv.bg>
2022-03-10 13:58:59 +0200

    Created a view model for NewMovieDropdown menu.

---
+++ MovieTickets/MovieTickets.Services/ViewModel/NewMovieDropdownsVM.cs
@@ -0,0 +1,18 @@
+﻿using MovieTickets.Data.Models;
+
+namespace MovieTickets.Services.ViewModel
+{
+    public class NewMovieDropdownsVM
+    {
+        public NewMovieDropdownsVM()
+        {
+            Producers = new List<Producer>();
+            Cinemas = new List<Cinema>();
+            Actors = new List<Actor>();
+        }
+
+        public List<Producer> Producers { get; set; }
+        public List<Cinema> Cinemas { get; set; }
+        public List<Actor> Actors { get; set; }
+    }
+}

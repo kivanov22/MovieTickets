a400c053feb93a8bde973159cfefc7ae96dde55d
Kristian Ivanov <kriskopk@abv.bg>
2022-03-12 19:53:34 +0200

    Created a shopping cart class which has context as property, shopping card Id property and list of shopping cart items. Also created a method which gets all shopping card items.

---
+++ MovieTickets/MovieTickets.Services/Cart/ShoppingCart.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MovieTickets.Data;
+using MovieTickets.Data.Models;
+
+namespace MovieTickets.Services.Cart
+{
+    public class ShoppingCart
+    {
+        public MovieTicketsDbContext _context { get; set; }
+
+        public string ShoppingCartId { get; set; }
+
+        public List<ShoppingCartItem> ShoppingCartItems { get; set; }
+
+        public ShoppingCart(MovieTicketsDbContext context)
+        {
+            _context = context;
+        }
+
+        public List<ShoppingCartItem> GetShoppingCartItems()
+        {
+            return ShoppingCartItems ?? (ShoppingCartItems = _context.ShoppingCartItems.Where(n => n.ShoppingCartId == ShoppingCartId)
+                .Include(n => n.Movie).ToList());
+        }
+    }
+}

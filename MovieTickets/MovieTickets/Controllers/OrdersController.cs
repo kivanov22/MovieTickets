2ed730aa44bfc86183ea3a8e4e2e6e1f67d97676
Kristian Ivanov <kriskopk@abv.bg>
2022-03-12 20:30:23 +0200

    Created Order controller and injected movie service interface and shopping cart. And Index method which returns all the items in the cart.

--- MovieTickets/MovieTickets/Controllers/OrdersController.cs
+++ MovieTickets/MovieTickets/Controllers/OrdersController.cs
@@ -1,6 +1,35 @@
-﻿namespace MovieTickets.Web.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieTickets.Services.Cart;
+using MovieTickets.Services.Contracts;
+using MovieTickets.Web.ViewModels.Orders;
+
+namespace MovieTickets.Web.Controllers
 {
-    public class OrdersController
+    public class OrdersController:Controller
     {
+        private readonly IMovieService _moviesService;
+        private readonly ShoppingCart _shoppingCart;
+
+        public OrdersController(IMovieService moviesService,
+            ShoppingCart shoppingCart)
+        {
+            _moviesService = moviesService;
+            _shoppingCart = shoppingCart;
+        }
+
+        public IActionResult Index()
+        {
+            var items = _shoppingCart.GetShoppingCartItems();
+
+            _shoppingCart.ShoppingCartItems = items; 
+
+            var response = new ShoppingCartViewModel()
+            {
+                ShoppingCart = _shoppingCart,
+                ShoppingCartTotal = _shoppingCart.GetShoppingCartTotal()
+            };
+
+            return View(response);
+        }
     }
 }

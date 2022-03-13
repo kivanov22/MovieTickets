2e96074148d25c45a42b3427842afb303acdbdbe
Kristian Ivanov <kriskopk@abv.bg>
2022-03-13 16:01:00 +0200

    Changed the location of ShoppingCart class, from services to web.Because can't access the Session and HttpContext.And created a method which uses the session and context to store the Shopping cart.

--- MovieTickets/MovieTickets/ViewModels/Orders/ShoppingCart.cs
+++ MovieTickets/MovieTickets/ViewModels/Orders/ShoppingCart.cs
@@ -1,6 +1,89 @@
-﻿namespace MovieTickets.Web.ViewModels.Orders
+﻿using Microsoft.EntityFrameworkCore;
+using MovieTickets.Data;
+using MovieTickets.Data.Models;
+
+namespace MovieTickets.Web.ViewModels.Orders
 {
     public class ShoppingCart
     {
+        //private readonly IHttpContextAccessor _httpContextAccessor;
+        public MovieTicketsDbContext _context { get; set; }
+
+        public string ShoppingCartId { get; set; }
+
+        public List<ShoppingCartItem> ShoppingCartItems { get; set; }
+
+        public ShoppingCart(MovieTicketsDbContext context)
+        {
+            _context = context;
+        }
+
+        public static ShoppingCart GetShoppingCart(IServiceProvider services)
+        {
+            ISession session = services.GetRequiredService<IHttpContextAccessor>()?.HttpContext.Session;
+
+            var context = services.GetService<MovieTicketsDbContext>();
+
+            string cartId = session.GetString("CartId") ?? Guid.NewGuid().ToString();
+            session.SetString("CartId", cartId);
+
+            return new ShoppingCart(context) { ShoppingCartId = cartId };
+        }
+
+        public void AddItemToCart(Movie movie)
+        {
+            var shoppingCartItem = _context.ShoppingCartItems.FirstOrDefault(m => m.Movie.Id == movie.Id &&
+            m.ShoppingCartId == ShoppingCartId);
+
+            if (shoppingCartItem == null)
+            {
+                shoppingCartItem = new ShoppingCartItem()
+                {
+                    ShoppingCartId = ShoppingCartId,
+                    Movie = movie,
+                    Quantity = 1
+                };
+
+                _context.ShoppingCartItems.Add(shoppingCartItem);
+            }
+            else
+            {
+                shoppingCartItem.Quantity++;
+            }
+            _context.SaveChanges();
+        }
+
+        public void RemoveItemFromCart(Movie movie)
+        {
+            var shoppingCartItem = _context.ShoppingCartItems.FirstOrDefault(m => m.Movie.Id == movie.Id &&
+           m.ShoppingCartId == ShoppingCartId);
+
+            if (shoppingCartItem != null)
+            {
+                if (shoppingCartItem.Quantity > 1)
+                {
+                    shoppingCartItem.Quantity--;
+                }
+                else
+                {
+                    _context.ShoppingCartItems.Remove(shoppingCartItem);
+                }
+            }
+            _context.SaveChanges();
+        }
+
+        public List<ShoppingCartItem> GetShoppingCartItems()
+        {
+            return ShoppingCartItems ?? (ShoppingCartItems = _context.ShoppingCartItems.Where(s => s.ShoppingCartId == ShoppingCartId)
+                .Include(m => m.Movie).ToList());
+        }
+
+        public double GetShoppingCartTotal()
+        {
+            var total = _context.ShoppingCartItems.Where(s => s.ShoppingCartId == ShoppingCartId)
+                .Select(s => s.Movie.Price * s.Quantity).Sum();
+
+            return total;
+        }
     }
 }

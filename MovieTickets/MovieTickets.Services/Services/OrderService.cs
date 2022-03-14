3dde97081d4f607df811f6a66f97c6de3ce0e829
Kristian Ivanov <kriskopk@abv.bg>
2022-03-14 18:01:48 +0200

    Created Order service class which inherits the IOrderService and implemented the two methods. For getting orders of user by userId .

--- MovieTickets/MovieTickets.Services/Services/OrderService.cs
+++ MovieTickets/MovieTickets.Services/Services/OrderService.cs
@@ -1,8 +1,33 @@
-﻿using MovieTickets.Services.Contracts;
+﻿using Microsoft.EntityFrameworkCore;
+using MovieTickets.Data;
+using MovieTickets.Data.Models;
+using MovieTickets.Services.Contracts;
 
 namespace MovieTickets.Services.Services
 {
-    public class OrderService:IOrderService
+    public class OrderService : IOrderService
     {
+        private readonly MovieTicketsDbContext _context;
+
+        public OrderService(MovieTicketsDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Order>> GetOrdersByUserIdAsync(string userId)
+        {
+            var orders = await _context.Orders
+                .Include(o => o.OrderItems)
+                .ThenInclude(m => m.Movie)
+                .Where(u => u.UserId == userId)
+                .ToListAsync();
+
+            return orders;
+        }
+
+        public Task StoreOrderAsync(List<ShoppingCartItem> items, string userId, string userEmailAddress)
+        {
+            throw new NotImplementedException();
+        }
     }
 }

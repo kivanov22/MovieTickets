fa76d13d6e263b02820a426cba9d2a01d1068179
Kristian Ivanov <kriskopk@abv.bg>
2022-03-14 15:40:36 +0200

    Created a remove item from cart method. Which find the movie by id and if not null removes it from cart.

--- MovieTickets/MovieTickets/Controllers/OrdersController.cs
+++ MovieTickets/MovieTickets/Controllers/OrdersController.cs
@@ -31,13 +31,24 @@ namespace MovieTickets.Web.Controllers
             return View(response);
         }
 
-        public async Task<RedirectToActionResult> AddToShoppingCart(int id)
+        public async Task<IActionResult> AddToShoppingCart(int id)
         {
             var item = await _moviesService.GetByIdAsync(id);
 
             if (item != null)
             {
                 _shoppingCart.AddItemToCart(item);
+            }
+            return RedirectToAction(nameof(ShoppingCart));
+        }
+
+        public async Task<IActionResult> RemoveItemFromShoppingCart(int id)
+        {
+            var item = await _moviesService.GetByIdAsync(id);
+
+            if (item != null)
+            {
+                _shoppingCart.RemoveItemFromCart(item);
             }
             return RedirectToAction(nameof(ShoppingCart));
         }

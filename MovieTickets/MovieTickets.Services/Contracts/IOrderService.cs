0b883c1c742ea594791b4e1b1c9c7d438c5d2eb0
Kristian Ivanov <kriskopk@abv.bg>
2022-03-15 17:56:41 +0200

    Added userRole to the GetOrdersByUser method.

--- MovieTickets/MovieTickets.Services/Contracts/IOrderService.cs
+++ MovieTickets/MovieTickets.Services/Contracts/IOrderService.cs
@@ -6,6 +6,6 @@ namespace MovieTickets.Services.Contracts
     {
         Task StoreOrderAsync(List<ShoppingCartItem> items, string userId, string userEmailAddress);
 
-        Task<List<Order>> GetOrdersByUserIdAsync(string userId);
+        Task<List<Order>> GetOrdersByUserIdAndRoleAsync(string userId,string userRole);
     }
 }

76f0eea7ed3fd49c17843ef420dd633c711c931d
Kristian Ivanov <kriskopk@abv.bg>
2022-03-15 18:54:11 +0200

    New migration.

---
+++ MovieTickets/MovieTickets.Data/Data/Migrations/20220315160625_WorkingVersion.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MovieTickets.Data.Data.Migrations
+{
+    public partial class WorkingVersion : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}

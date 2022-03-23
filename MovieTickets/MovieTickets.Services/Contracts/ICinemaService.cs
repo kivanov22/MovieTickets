23be6c107ad3de10ab0d66d98d3acc9fb7312f19
Kristian Ivanov <kriskopk@abv.bg>
2022-03-23 13:23:55 +0200

    Created custom methods for getbyid,add new and update. Delete is going to be added too.

--- MovieTickets/MovieTickets.Services/Contracts/ICinemaService.cs
+++ MovieTickets/MovieTickets.Services/Contracts/ICinemaService.cs
@@ -1,9 +1,15 @@
 ﻿using MovieTickets.Data.Data.Common;
 using MovieTickets.Data.Models;
+using MovieTickets.Services.ViewModel.Cinemas;
 
 namespace MovieTickets.Services.Contracts
 {
     public interface ICinemaService:IEntityBaseRepository<Cinema>
     {
+        Task<Cinema> GetCinemaByIdAsync(int id);
+        Task AddNewCinemaAsync(CinemaViewModel data);
+        Task UpdateCinemaAsync(CinemaViewModel data);
+
+        Task DeleteCinemaAsync(int id);
     }
 }
--- MovieTickets/MovieTickets.Services/Contracts/IProducerService.cs
+++ MovieTickets/MovieTickets.Services/Contracts/IProducerService.cs
@@ -1,9 +1,13 @@
 ﻿using MovieTickets.Data.Data.Common;
 using MovieTickets.Data.Models;
+using MovieTickets.Services.ViewModel.Producers;
 
 namespace MovieTickets.Services.Contracts
 {
     public interface IProducerService : IEntityBaseRepository<Producer>
     {
+        Task<Producer> GetProducerByIdAsync(int id);
+        Task AddNewProducerAsync(ProducerViewModel data);
+        Task UpdateProducerAsync(ProducerViewModel data);
     }
 }

a90a523d8fabc7418e1d725a7bb50653df11fe08
Kristian Ivanov <kriskopk@abv.bg>
2022-03-23 13:23:10 +0200

    Created custom methods for getbyid,add new actor and update. Delete is going to be added too.

--- MovieTickets/MovieTickets.Services/Contracts/IActorService.cs
+++ MovieTickets/MovieTickets.Services/Contracts/IActorService.cs
@@ -1,9 +1,19 @@
 ﻿using MovieTickets.Data.Data.Common;
 using MovieTickets.Data.Models;
+using MovieTickets.Services.ViewModel.Actors;
 
 namespace MovieTickets.Services.Contracts
 {
     public interface IActorService:IEntityBaseRepository<Actor>
     {
+        Task<Actor> GetActorByIdAsync(int id);
+        Task AddNewActorAsync(ActorViewModel data);
+        Task UpdateActorAsync(ActorViewModel data);
+
+        //Task<NewMovieDropdownsVM> GetNewMovieDropdownsValues();
+
+
+        //delete possible
+        //Task DeleteActorAsync(int id);
     }
 }

6acf574cb93c36d5950fdc4ac465b95804dc7d22
Kristian Ivanov <kriskopk@abv.bg>
2022-04-10 20:25:03 +0300

    Created a service extension for the services and db context.

---
+++ MovieTickets/MovieTickets/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MovieTickets.Data;
+using MovieTickets.Services.Contracts;
+using MovieTickets.Services.Services;
+using MovieTickets.Web.ViewModels.Orders;
+
+namespace MovieTickets.Web.Extensions
+{
+    public static class ServiceCollectionExtension
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddScoped<IActorService, ActorService>();
+            services.AddScoped<IProducerService, ProducerService>();
+            services.AddScoped<IMovieService, MovieService>();
+            services.AddScoped<ICinemaService, CinemaService>();
+            services.AddScoped<IOrderService, OrderService>();
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddScoped(sc => ShoppingCart.GetShoppingCart(sc));
+            return services;
+        }
+
+        public static IServiceCollection AddApplicationDbContexts(this IServiceCollection services, IConfiguration config)
+        {
+            var connectionString = config.GetConnectionString("DefaultConnection");
+            services.AddDbContext<MovieTicketsDbContext>(options =>
+                options.UseSqlServer(connectionString));
+            services.AddDatabaseDeveloperPageExceptionFilter();
+
+            return services;
+        }
+    }
+}
